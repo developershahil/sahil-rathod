ad360beb104ba10bb48cdd2179a3d77355adc3c0
Rathod Sahil <sahilrathod222@gmail.com>
2026-08-08 19:30:52 +0530

    initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "sahil-portfolio";
+const repoName = "sahil-rathod";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
